f4cdafd04ca852390504525abe74685035af0411
Ionică Bizău <bizauionica@gmail.com>
2016-03-17 18:55:55 +0200

    :rocket:

--- README.md
+++ README.md
@@ -16,9 +16,53 @@ $ npm i --save ansi-to-json
 "use strict";
 
 const ansiToJson = require("ansi-to-json");
+console.log(JSON.stringify(ansiToJson("\u001b[38;5;196mHello\u001b[39m \u001b[48;5;226mWorld\u001b[49m"), null, 4));
+// [
+//     {
+//         "content": "Hello",
+//         "fg": "255, 0, 0",
+//         "bg": null,
+//         "fg_truecolor": null,
+//         "bg_truecolor": null,
+//         "was_processed": true
+//     },
+//     {
+//         "content": " ",
+//         "fg": null,
+//         "bg": null,
+//         "fg_truecolor": null,
+//         "bg_truecolor": null,
+//         "was_processed": false
+//     },
+//     {
+//         "content": "World",
+//         "fg": null,
+//         "bg": "255, 255, 0",
+//         "fg_truecolor": null,
+//         "bg_truecolor": null,
+//         "was_processed": true
+//     }
+// ]
 
-console.log(ansiToJson("\x1b[30mblack\x1b[37mwhite"));
-console.log(ansiToJson("\u001b[1mfoo\u001b[22m"));
+console.log(JSON.stringify(ansiToJson("\x1b[30mblack\x1b[37mwhite"), null, 4));
+// [
+//     {
+//         "content": "black",
+//         "fg": "0, 0, 0",
+//         "bg": null,
+//         "fg_truecolor": null,
+//         "bg_truecolor": null,
+//         "was_processed": true
+//     },
+//     {
+//         "content": "white",
+//         "fg": "255,255,255",
+//         "bg": null,
+//         "fg_truecolor": null,
+//         "bg_truecolor": null,
+//         "was_processed": true
+//     }
+// ]
 ```
 
 ## Documentation
--- example/index.js
+++ example/index.js
@@ -1,35 +1,50 @@
 "use strict";
 
 const ansiToJson = require("../lib");
+console.log(JSON.stringify(ansiToJson("\u001b[38;5;196mHello\u001b[39m \u001b[48;5;226mWorld\u001b[49m"), null, 4));
+// [
+//     {
+//         "content": "Hello",
+//         "fg": "255, 0, 0",
+//         "bg": null,
+//         "fg_truecolor": null,
+//         "bg_truecolor": null,
+//         "was_processed": true
+//     },
+//     {
+//         "content": " ",
+//         "fg": null,
+//         "bg": null,
+//         "fg_truecolor": null,
+//         "bg_truecolor": null,
+//         "was_processed": false
+//     },
+//     {
+//         "content": "World",
+//         "fg": null,
+//         "bg": "255, 255, 0",
+//         "fg_truecolor": null,
+//         "bg_truecolor": null,
+//         "was_processed": true
+//     }
+// ]
 
 console.log(JSON.stringify(ansiToJson("\x1b[30mblack\x1b[37mwhite"), null, 4));
 // [
 //     {
-//         "color": {
-//             "fg": "0, 0, 0",
-//             "bg": null
-//         },
 //         "content": "black",
-//         "html": {
-//             "styles": [
-//                 "color:rgb(0, 0, 0)"
-//             ],
-//             "attributes": ""
-//         },
-//         "attributes": {}
+//         "fg": "0, 0, 0",
+//         "bg": null,
+//         "fg_truecolor": null,
+//         "bg_truecolor": null,
+//         "was_processed": true
 //     },
 //     {
-//         "color": {
-//             "fg": "255,255,255",
-//             "bg": null
-//         },
 //         "content": "white",
-//         "html": {
-//             "styles": [
-//                 "color:rgb(255,255,255)"
-//             ],
-//             "attributes": ""
-//         },
-//         "attributes": {}
+//         "fg": "255,255,255",
+//         "bg": null,
+//         "fg_truecolor": null,
+//         "bg_truecolor": null,
+//         "was_processed": true
 //     }
 // ]
